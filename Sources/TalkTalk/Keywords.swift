86e1e6ab58ee88acb0e6147d530355b5c0d8b355
Pat Nakajima <patnakajima@gmail.com>
2024-07-03 19:57:52 -0700

    this wants to be a class

--- Sources/TalkTalk/Keywords.swift
+++ Sources/TalkTalk/Keywords.swift
@@ -4,8 +4,8 @@
 //
 //  Created by Pat Nakajima on 7/1/24.
 //
-struct KeywordTrie: @unchecked Sendable {
-	struct Node: @unchecked Sendable {
+class KeywordTrie: @unchecked Sendable {
+	class Node: @unchecked Sendable {
 		var keyword: Token.Kind?
 		var children: [Character: Node]
 
