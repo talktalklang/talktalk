79fcc15779fbfcfb5b9575d9bb497ababd59f0dd
Pat Nakajima <patnakajima@gmail.com>
2024-07-07 23:36:08 -0700

    update grammar

--- tree-sitter-talktalk/grammar.js
+++ tree-sitter-talktalk/grammar.js
@@ -19,209 +19,230 @@ module.exports = grammar({
     )),
 
     declaration: $ => choice(
-      $.function_declaration,
-      $.variable_declaration,
-      $.class_declaration,
+      $.classDecl,
+      $.funcDecl,
+      $.varDecl,
       $.statement
     ),
 
-    function_declaration: $ => seq(
-      'func',
+    classDecl: $ => seq(
+      "class",
       field('name', $.identifier),
-      '(',
-      optional($.parameters),
-      ')',
-      $.block
-    ),
-
-    init_declaration: $ => seq(
-      'init',
-      '(',
-      optional($.parameters),
-      ')',
-      $.block
-    ),
-
-    class_declaration: $ => seq(
-      'class',
-      field('name', $.identifier),
-      '{',
+      optional(
+        seq(":", $.identifier)
+      ),
+      "{",
       repeat(
         choice(
-          $.init_declaration,
-          $.function_declaration
+          $.init,
+          $.function
         )
       ),
-      '}'
+      "}"
+    ),
+
+    funcDecl: $ => seq(
+      "func",
+      $.function
+    ),
+
+    varDecl: $ => seq(
+      "var",
+      $.identifier,
+      optional(
+        seq("=", $.expr)
+      )
+    ),
+
+    statement: $ => choice(
+      $.exprStmt,
+      $.ifStmt,
+      $.returnStmt,
+      $.whileStmt,
+      $.block
+    ),
+
+    exprStmt: $ => $.expr,
+
+    ifStmt: $ => seq(
+      "if",
+      $.expr,
+      $.block,
+      optional(seq(
+        "else",
+        $.statement
+      ))
+    ),
+
+    returnStmt: $ => seq(
+      "return",
+      optional(
+        $.expr
+      ),
+      "\n"
+    ),
+
+    whileStmt: $ => seq(
+      "while",
+      $.expr,
+      $.block
     ),
 
     block: $ => seq(
       "{",
-      repeat($.declaration),
+      repeat(
+        $.declaration
+      ),
       "}"
     ),
 
-    statement: $ => choice(
-      $.print_statement,
-      $.expression_statement,
-      $.assignment_statement,
-      $.block,
-      $.if_statement,
-      $.while_statement,
-      $.return_statement
+    expr: $ => $.assignment,
+
+    assignment: $ => choice(
+      seq(
+        optional(
+          seq(
+            $.call,
+            "."
+          )
+        ),
+        $.identifier,
+        "=",
+        $.assignment
+      ),
+      $.logic_or
     ),
 
-    if_statement: $ => seq(
-      'if',
-      $.expression,
-      $.block,
-      optional($.else_statement)
+    logic_or: $ => seq(
+      $.logic_and,
+      repeat(
+        seq(
+          "||",
+          $.logic_and
+        )
+      )
     ),
 
-    else_statement: $ => seq(
-      'else',
+    logic_and: $ => seq(
+      $.equality,
+      repeat(
+        seq(
+          "&&",
+          $.equality
+        )
+      )
+    ),
+
+    equality: $ => seq(
+      $.comparison,
+      repeat(
+        seq(
+          choice("!=", "=="),
+          $.comparison
+        )
+      )
+    ),
+
+    comparison: $ => seq(
+      $.term,
+      repeat(
+        seq(
+          choice(
+            ">",
+            ">=",
+            "<",
+            "<="
+          ),
+          $.term
+        )
+      )
+    ),
+
+    term: $ => prec.left(seq(
+      $.factor,
+      repeat(
+        seq(
+          choice("-", "+"),
+          $.factor
+        )
+      )
+    )),
+
+    factor: $ => prec.left(seq(
+      $.unary,
+      repeat(
+        seq(
+          choice("/", "*"),
+          $.factor
+        )
+      )
+    )),
+
+    unary: $ => choice(
+      seq(
+        choice(
+          "!", "-"
+        ),
+        choice($.unary, $.call)
+      )
+    ),
+
+    call: $ => prec.left(seq(
+      $.primary,
+      repeat(
+        choice(
+          seq("(", optional($.arguments), ")"),
+          seq(".", $.identifier)
+        )
+      )
+    )),
+
+    primary: $ => choice(
+      "true", "false", "nil", "self", $.number,
+      $.string, $.identifier,
+      seq("(", $.expr, ")"),
+      seq("super", ".", $.identifier)
+    ),
+
+    function: $ => seq(
+      field('name', $.identifier),
+      "(",
+      optional($.parameters),
+      ")",
       $.block
     ),
 
-    return_statement: $ => seq(
-      'return',
-      $.expression
-    ),
-
-    func: $ => seq(
-      field('name', $.identifier),
-      '(',
+    init: $ => seq(
+      "init",
+      "(",
       optional($.parameters),
-      ')',
+      ")",
       $.block
     ),
 
     parameters: $ => seq(
       $.identifier,
-      repeat(seq(',', $.identifier))
-    ),
-
-    identifier: _ => /[a-zA-Z_]\w*/,
-
-    equality: $ => seq(
-      $.comparison,
-      repeat(seq(choice('==', '!='), $.comparison))
-    ),
-
-    comparison: $ => seq(
-      $.term,
-      repeat(seq(choice('>', '>=', '<', '<='), $.term))
-    ),
-
-    term: $ => seq(
-      $.factor,
-      repeat(seq(choice('+', '-'), $.factor))
-    ),
-
-    factor: $ => seq(
-      $.unary_expression,
-      repeat(seq(choice('*', '/'), $.unary_expression))
-    ),
-
-    logic_or: $ => seq(
-      $.logic_and,
-      repeat(seq('||', $.equality))
-    ),
-
-    logic_and: $ => seq(
-      $.equality,
-      repeat(seq('&&', $.equality))
-    ),
-
-    while_statement: $ => seq(
-      'while',
-      $.expression,
-      $.block
-    ),
-
-    expression_statement: $ => seq(
-      $.expression,
-      ';'
-    ),
-
-    expression: $ => choice(
-      $.binary_expression,
-      $.unary_expression,
-      $.primary_expression,
-      $.grouped_expression
-    ),
-
-    grouped_expression: $ => seq(
-      '(',
-      $.expression,
-      ')'
-    ),
-
-    binary_expression: $ => prec.left(1, seq(
-      $.expression,
-      $.binary_operator,
-      $.expression
-    )),
-
-    print_statement: $ => seq(
-      'print',
-      $.expression,
-      ';'
-    ),
-
-    assignment_statement: $ => seq(
-      optional(
-        seq($.call, '.')
-      ),
-      $.identifier,
-      '=',
-      $.expression,
-      ';'
-    ),
-
-    unary_expression: $ => prec.left(2, seq(
-      choice('-', '!'),
-      $.call
-    )),
-
-    primary_expression: $ => choice(
-      $.number_literal,
-      'self',
-      'nil',
-      $.string_literal,
-      $.boolean_literal,
-      $.identifier,
-    ),
-
-    variable_declaration: $ => seq(
-      'var',
-      $.identifier,
-      '=',
-      $.expression,
-      ';'
-    ),
-
-    call: $ => prec.left(2,
-      seq(
-        $.primary_expression,
-        repeat(
-          choice(
-            seq('(', optional($.arguments), ')' ),
-            seq('.', $.identifier)
-          )
+      repeat(
+        seq(
+          ",",
+          $.identifier
         )
       )
     ),
 
-    arguments: $ => seq($.expression, repeat(seq(',', $.expression))),
+    arguments: $ => seq(
+      $.expr,
+      repeat(
+        seq(
+          ",",
+          $.expr
+        )
+      )
+    ),
 
-    binary_operator: _ => choice('+', '-', '*', '/', '==', '!=', '<', '<=', '>', '>='),
-
-    boolean_literal: _ => choice('true', 'false'),
-
-    number_literal: _ => /\d+(\.\d+)?/,
-
-    string_literal: _ => /"[^"]*"/,
+    string: _ => /"[^"]*"/,
+    number: _ => /\d+/,
+    identifier: _ => /[a-zA-Z_]\w*/,
   }
 });
 
--- tree-sitter-talktalk/queries/highlights.scm
+++ tree-sitter-talktalk/queries/highlights.scm
@@ -1,6 +1,5 @@
 [
 "var"
-"print"
 "if"
 "else"
 "while"
@@ -11,9 +10,9 @@
 ] @keyword
 (comment) @comment
 (call) @function.call
-(function_declaration name: (identifier)) @function.name
-(string_literal) @string
-(number_literal) @number
-(class_declaration name: (identifier)) @type
+(function name: (identifier)) @function.name
+(string) @string
+(number) @number
+(classDecl name: (identifier)) @type
 (parameters) @function.parameters
-(init_declaration (block)) @constructor
+(init (block)) @constructor
