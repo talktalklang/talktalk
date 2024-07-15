282ec98bb8539d8d400ac6125c7279771c4ced2e
Pat Nakajima <patnakajima@gmail.com>
2024-07-15 05:58:51 +0000

    wip

--- Sources/TalkTalkSyntax/GenericVisitor.swift
+++ Sources/TalkTalkSyntax/GenericVisitor.swift
@@ -1,192 +1,203 @@
 //
 //  GenericVisitor.swift
-//  
+//
 //
 //  Created by Pat Nakajima on 7/14/24.
 //
-public struct GenericVisitor<Value>: ASTVisitor {
-	public struct Context {
-		var depth: Int = 0
-
-		func nest() -> Context {
-			var copy = self
-			copy.depth += 1
-			return copy
-		}
-	}
-
-	let perform: (any Syntax, Context) -> Value
+public struct GenericVisitor<Context>: ASTVisitor {
+	let perform: (any Syntax, Context) -> Context
 
 	// MARK: Visits
 
-	mutating func visit(_ expr: any Syntax, context: Context) -> Value {
-		_ = perform(expr, context)
-		_ = expr.accept(&self, context: context)
+	mutating func visit(_ expr: any Syntax, context: Context) -> Context {
+		var context = perform(expr, context)
+		context = expr.accept(&self, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: ProgramSyntax, context: Context) -> Value {
-		let result = perform(node, context)
+	public mutating func visit(_ node: ProgramSyntax, context: Context) -> Context {
+		var context = perform(node, context)
 		for decl in node.decls {
-			_ = decl.accept(&self, context: context.nest())
+			context = decl.accept(&self, context: context)
 		}
-		return result
+		return context
 	}
 
-	public mutating func visit(_ node: FunctionDeclSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.name, context: context.nest())
-		_ = visit(node.parameters, context: context.nest())
+	public mutating func visit(_ node: FunctionDeclSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.name, context: context)
+		context = visit(node.parameters, context: context)
 		if let typeDecl = node.typeDecl {
-			_ = visit(typeDecl, context: context.nest())
+			context = visit(typeDecl, context: context)
 		}
-		_ = visit(node.body, context: context.nest())
+		context = visit(node.body, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: VarDeclSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.variable, context: context.nest())
+	public mutating func visit(_ node: VarDeclSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.variable, context: context)
 
 		if let expr = node.expr {
-			_ = expr.accept(&self, context: context.nest())
+			_ = expr.accept(&self, context: context)
 		}
+
+		return context
 	}
 
-	public mutating func visit(_ node: ClassDeclSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.name, context: context)
-		_ = visit(node.body, context: context)
+	public mutating func visit(_ node: ClassDeclSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.name, context: context)
+		context = visit(node.body, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: InitDeclSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.parameters, context: context)
-		_ = visit(node.body, context: context)
+	public mutating func visit(_ node: InitDeclSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.parameters, context: context)
+		context = visit(node.body, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: PropertyDeclSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.name, context: context)
-		_ = visit(node.typeDecl, context: context)
+	public mutating func visit(_ node: PropertyDeclSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.name, context: context)
+		context = visit(node.typeDecl, context: context)
 
 		if let value = node.value {
-			_ = visit(value, context: context)
+			context = visit(value, context: context)
 		}
+
+		return context
 	}
 
-	public mutating func visit(_ node: ExprStmtSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.expr, context: context)
+	public mutating func visit(_ node: ExprStmtSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.expr, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: BlockStmtSyntax, context: Context) -> Value {
-		_ = perform(node, context)
+	public mutating func visit(_ node: BlockStmtSyntax, context: Context) -> Context {
+		var context = perform(node, context)
 		for decl in node.decls {
-			_ = visit(decl, context: context.nest())
+			context = visit(decl, context: context)
 		}
+
+		return context
 	}
 
-	public mutating func visit(_ node: IfStmtSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.condition, context: context)
-		_ = visit(node.body, context: context)
+	public mutating func visit(_ node: IfStmtSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.condition, context: context)
+		context = visit(node.body, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: StmtSyntax, context: Context) -> Value {
-		_ = perform(node, context)
+	public mutating func visit(_ node: StmtSyntax, context: Context) -> Context {
+		perform(node, context)
 	}
 
-	public mutating func visit(_ node: WhileStmtSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.condition, context: context)
-		_ = visit(node.body, context: context)
+	public mutating func visit(_ node: WhileStmtSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.condition, context: context)
+		context = visit(node.body, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: ReturnStmtSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.value, context: context)
+	public mutating func visit(_ node: ReturnStmtSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.value, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: GroupExpr, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.expr, context: context)
+	public mutating func visit(_ node: GroupExpr, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.expr, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: CallExprSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.callee, context: context)
-		_ = visit(node.arguments, context: context)
+	public mutating func visit(_ node: CallExprSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.callee, context: context)
+		context = visit(node.arguments, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: UnaryExprSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.op, context: context)
-		_ = visit(node.rhs, context: context)
+	public mutating func visit(_ node: UnaryExprSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.op, context: context)
+		context = visit(node.rhs, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: BinaryExprSyntax, context: Context) -> Value {
-		_ = perform(node, context)
-		_ = visit(node.lhs, context: context)
-		_ = visit(node.op, context: context)
-		_ = visit(node.rhs, context: context)
+	public mutating func visit(_ node: BinaryExprSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.lhs, context: context)
+		context = visit(node.op, context: context)
+		context = visit(node.rhs, context: context)
+		return context
 	}
 
-	public mutating func visit(_ node: IdentifierSyntax, context: Context) -> Value {
-		_ = perform(node, context)
+	public mutating func visit(_ node: IdentifierSyntax, context: Context) -> Context {
+		perform(node, context)
 	}
 
-	public mutating func visit(_ node: IntLiteralSyntax, context: Context) -> Value {
+	public mutating func visit(_ node: IntLiteralSyntax, context: Context) -> Context {
+		perform(node, context)
+	}
+
+	public mutating func visit(_ node: StringLiteralSyntax, context: Context) -> Context {
+		perform(node, context)
+	}
+
+	public mutating func visit(_ node: VariableExprSyntax, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.name, context: context)
+		return context
+	}
+
+	public mutating func visit(_ node: AssignmentExpr, context: Context) -> Context {
+		var context = perform(node, context)
+		context = visit(node.lhs, context: context)
+	}
+
+	public mutating func visit(_ node: LiteralExprSyntax, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: StringLiteralSyntax, context: Context) -> Value {
+	public mutating func visit(_ node: PropertyAccessExpr, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: VariableExprSyntax, context: Context) -> Value {
+	public mutating func visit(_ node: ArrayLiteralSyntax, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: AssignmentExpr, context: Context) -> Value {
+	public mutating func visit(_ node: IfExprSyntax, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: LiteralExprSyntax, context: Context) -> Value {
+	public mutating func visit(_ node: UnaryOperator, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: PropertyAccessExpr, context: Context) -> Value {
+	public mutating func visit(_ node: BinaryOperatorSyntax, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: ArrayLiteralSyntax, context: Context) -> Value {
+	public mutating func visit(_ node: ArgumentListSyntax, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: IfExprSyntax, context: Context) -> Value {
+	public mutating func visit(_ node: ParameterListSyntax, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: UnaryOperator, context: Context) -> Value {
+	public mutating func visit(_ node: ErrorSyntax, context: Context) -> Context {
 		<#code#>
 	}
 
-	public mutating func visit(_ node: BinaryOperatorSyntax, context: Context) -> Value {
-		<#code#>
-	}
-
-	public mutating func visit(_ node: ArgumentListSyntax, context: Context) -> Value {
-		<#code#>
-	}
-
-	public mutating func visit(_ node: ParameterListSyntax, context: Context) -> Value {
-		<#code#>
-	}
-
-	public mutating func visit(_ node: ErrorSyntax, context: Context) -> Value {
-		<#code#>
-	}
-
-	public mutating func visit(_ node: TypeDeclSyntax, context: Context) -> Value {
+	public mutating func visit(_ node: TypeDeclSyntax, context: Context) -> Context {
 		<#code#>
 	}
 }
