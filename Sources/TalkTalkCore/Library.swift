89ca19c387c87044a61a1d2e8b626ed51f81e553
Pat Nakajima <patnakajima@gmail.com>
2024-09-15 10:46:04 -0700

    add ability to override bundle url

--- Sources/TalkTalkCore/Library.swift
+++ Sources/TalkTalkCore/Library.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 public struct Library {
+	static let talktalkLibraryEnvKey = "TALKTALK_BUNDLE_PATH"
+
 	// The name of this library
 	public let name: String
 
@@ -21,9 +23,18 @@ public struct Library {
 // Helpers
 public extension Library {
 	static var libraryURL: URL {
-		// swiftlint:disable force_unwrapping
-		Bundle.module.resourceURL!
-		// swiftlint:enable force_unwrapping
+		if let bundlePath = ProcessInfo.processInfo.environment[talktalkLibraryEnvKey] {
+			if FileManager.default.fileExists(atPath: bundlePath), let url = URL(string: bundlePath) {
+				return url
+			} else {
+				print("Could not find bundle path from environment key: \(talktalkLibraryEnvKey)")
+				fatalError("No bundle found.")
+			}
+		} else {
+			// swiftlint:disable force_unwrapping
+			return Bundle.module.resourceURL!
+			// swiftlint:enable force_unwrapping
+		}
 	}
 
 	// This is the standard library. It's kind of a big deal.
