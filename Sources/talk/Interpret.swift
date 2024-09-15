25dad3b1398fc2aada9ba166f418e87828306eb6
Pat Nakajima <patnakajima@gmail.com>
2024-09-14 20:26:03 -0700

    boop

--- Sources/talk/Interpret.swift
+++ Sources/talk/Interpret.swift
@@ -10,7 +10,7 @@ import TalkTalkInterpreter
 
 struct Interpret: TalkTalkCommand {
 	static let configuration = CommandConfiguration(
-		abstract: "Run the given input in the tree walking interpreter"
+		abstract: "Run the given input in the tree walking interpreter (alpha)"
 	)
 
 	@Argument(help: "The input to interpret.", completion: .file(extensions: [".talk"]))
